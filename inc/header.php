5e09b229f4ac465bc9f6cd6311c8a3f406ae5105
ahmedsaadx <ahmeddsaadd188@gmail.com>
2024-10-15 12:04:14 +0300

    mv handlers to pages

--- inc/header.php
+++ inc/header.php
@@ -18,22 +18,17 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="../handlers/list_task.php">Tasks</a>
+                <a class="nav-link" href="../pages/list_task.php">Tasks</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="../handlers/create_task.php">Create Task</a>
+                <a class="nav-link" href="../pages/create_task.php">Create Task</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
     </header>
-
-    <!-- Bootstrap JS, Popper.js for functionality like dropdowns -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
-
-    <!-- Custom JavaScript -->
-    <script src="scripts/custom.js"></script> <!-- Add this if you have custom JS -->
   </body>
 </html>
